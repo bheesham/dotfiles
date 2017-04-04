6a023827eea1078609720c73b439b5b246f6f023
Bheesham Persaud <me@bheesham.com>
2017-04-04 15:28:15 -0400

    Misc updates.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
-fish_vi_key_bindings
-
 function try_add_path -d "Attempts to add a directory to $PATH."
   for path in $argv
     if not test -d $path
@@ -10,7 +8,7 @@ function try_add_path -d "Attempts to add a directory to $PATH."
   end
 end
 
-function fish_prompt
+function fish_prompt -d "Customize the fish prompt."
   echo (whoami) @ (hostname) / (basename (pwd))
   echo -n '$ '
 end
@@ -19,14 +17,25 @@ try_add_path /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin \
              $HOME/bin $HOME/.cargo/bin $HOME/.cabal/bin $HOME/.rvm/bin \
              $HOME/.local/bin \
              /usr/local/MacGPG2/bin /Library/TeX/texbin \
-	     /usr/local/opt/coreutils/libexec/gnubin \
+             /usr/local/opt/coreutils/libexec/gnubin \
              /usr/local/opt/gnu-sed/bin \
              /usr/lib/node_modules/express-generator/bin/
 
+if not test "$TERM" = "dumb"
+  fish_vi_key_bindings
+end
 
-set -x DEP_OPENSSL_INCLUDE /usr/local/opt/openssl/include
-set -x OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
-set -x OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
+if uname -a | grep -q Darwin
+  set -x DEP_OPENSSL_INCLUDE /usr/local/opt/openssl/include
+  set -x OPENSSL_INCLUDE_DIR /usr/local/opt/openssl/include
+  set -x OPENSSL_LIB_DIR /usr/local/opt/openssl/lib
+end
+
+for trout in $HOME/.config/work/extras.fish $HOME/.config/priv/extras.fish
+  if test -e $trout
+    source $trout
+  end
+end
 
 set -x EDITOR vim
 set -x HOMEBREW_EDITOR vim
